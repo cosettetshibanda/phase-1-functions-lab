ac6b58e80ef0077bf4cf0c6714f7154d4d0a2de8
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-11-29 14:48:23 -0700

    commit tree 6943b1eccc244df6730a58974dec3252a142309a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+ //returns the number of blocks given a value
+}
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264; 
+// call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start-destination) * 264)
+ //returns the number of feet traveled
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination)> 2500) {
+        return 'cannot travel that far'
+    }
+    //returns the fare for the customer
+  }
